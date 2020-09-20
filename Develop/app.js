2115bac8ad76d4a29183f541c482d690555a3899
Lindsey Smith <campbelllm@gmail.com>
2020-09-19 20:42:36 -0600

    questions working in order

--- Develop/app.js
+++ Develop/app.js
@@ -6,111 +6,160 @@ const path = require("path");
 const fs = require("fs");
 
 inquirer
-    .prompt([
-        //manager questions
-        {
-            type: 'input',
-            message: "Please enter the Managers name:",
-            name: 'managerName'
-        },
-        {
-            type: 'input',
-            message: "What is the Managers id?",
-            name: "managerID"
-        },
-        {
-            type: 'input',
-            message: 'What is the Managers email?',
-            name: 'managerEmail'
+  .prompt([
+    //manager questions
+    {
+      type: "input",
+      message: "Please enter the Managers name:",
+      name: "managerName",
+    },
+    {
+      type: "input",
+      message: "What is the Managers id?",
+      name: "managerID",
+    },
+    {
+      type: "input",
+      message: "What is the Managers email?",
+      name: "managerEmail",
+    },
+    {
+      type: "input",
+      message: "What is the Managers office number?",
+      name: "managerOfficeNumber",
+    },
+    //engineer questions
+    {
+      type: "list",
+      message: "Would you like to input an engineer?",
+      choices: ["yes", "no"],
+      name: "engineerYesNo",
+    },
+  ])
+  .then((answers) => {
+    //manager answers
+    const managerName = answers.managerName;
+    const managerID = answers.managerID;
+    const managerEmail = answers.managerEmail;
+    const managerOfficeNumber = answers.managerOfficeNumber;
+    //engineer answers
+    let engineerYesNo = answers.engineerYesNo;
+    let engineerName = answers.engineerName;
+    let engineerID = answers.engineerID;
+    let engineerEmail = answers.engineerEmail;
+    let engineerGitHub = answers.engineerGitHub;
+    //intern answer
+    let internYesNo = answers.internYesNo;
+    let internName = answers.internName;
+    let internID = answers.internID;
+    let internEmail = answers.internEmail;
+    let internSchool = answers.internSchool;
+    const internYes = () => {
+      if (internYesNo === "yes") {
+        inquirer
+          .prompt([
+            {
+              type: "input",
+              message: "Enter intern name:",
+              name: "internName",
+            },
+            {
+              type: "input",
+              message: "Enter intern id:",
+              name: "internId",
+            },
+            {
+              type: "input",
+              message: "Enter intern email:",
+              name: "internEmail",
+            },
+            {
+              type: "input",
+              message: "Enter intern school:",
+              name: "internSchool",
+            },
+            {
+              type: "list",
+              message: "Would you like to input another intern?",
+              choices: ["yes", "no"],
+              name: "internYesNo",
+            },
+          ])
+          .then((answers) => {
+            internYesNo = answers.internYesNo;
+            if (internYesNo === "yes") {
+              internYes();
+            }
+          });
+      }
+    };
 
-        },
-        {
-            type: 'input',
-            message: 'What is the Managers office number?',
-            name: 'managerOfficeNumber'
- 
-        },
-        //engineer questions
-        {
-            type: 'list',
-            message: 'Would you like to input an engineer?',
-            choices: ['yes', 'no'],
-            name: 'engineerYesNo'
-        },
-        {
-            type: 'input',
-            message: 'Enter engineer name:',
-            name: 'engineerName'
-        },
-        {
-            type: 'input',
-            message: 'Enter engineer id:',
-            name: 'engineerId'
-        },
-        {
-            type: 'input',
-            message: 'Enter engineer email:',
-            name: 'engineerEmail'
-        },
-        {
-            type: 'input',
-            message: 'Enter engineers GitHub username',
-            name: 'engineerGitHub'
-        }
-        // intern questions
-        {
-            type: 'list',
-            message: 'Would you like to input an intern?',
-            choices: ['yes', 'no'],
-            name: 'internYesNo'
-        },
-        {
-            type: 'input',
-            message: 'Enter intern name:',
-            name: 'internName'
-        },
-        {
-            type: 'input',
-            message: 'Enter intern id:',
-            name: 'internId'
-        },
-        {
-            type: 'input',
-            message: 'Enter intern email:',
-            name: 'internEmail'
-        },
-        {
-            type: 'input',
-            message: 'Enter intern school:',
-            name: 'internSchool'
-        }
-
-    ])
-    .then((answers) => {
-        //manager answers
-        const managerName = answers.managerName;
-        const managerID = answers.managerID;
-        const managerEmail = answers.managerEmail;
-        const managerOfficeNumber = answers.managerOfficeNumber;
-        //engineer answers
-        const engineerYesNo = answers.engineerYesNo;
-        const engineerName = answers.engineerName;
-        const engineerID = answers.engineerID;
-        const engineerEmail = answers.engineerEmail;
-        const engineerGitHub = answers.engineerGitHub;
-        //intern answer 
-        const internYesNo = answers.internYesNo;
-        const internName = answers.internName;
-        const internID = answers.internID;
-        const internEmail = answers.internEmail;
-        const internSchool = answers.internSchool;
-    })
+    const internPrompt = () => {
+      inquirer
+        .prompt([
+          {
+            type: "list",
+            message: "Would you like to input an intern?",
+            choices: ["yes", "no"],
+            name: "internYesNo",
+          },
+        ])
+        .then((answers) => {
+          internYesNo = answers.internYesNo;
+          internYes();
+        });
+    };
+    const engineerYes = () => {
+      if (engineerYesNo === "yes") {
+        inquirer
+          .prompt([
+            {
+              type: "input",
+              message: "Enter engineer name:",
+              name: "engineerName",
+            },
+            {
+              type: "input",
+              message: "Enter engineer id:",
+              name: "engineerId",
+            },
+            {
+              type: "input",
+              message: "Enter engineer email:",
+              name: "engineerEmail",
+            },
+            {
+              type: "input",
+              message: "Enter engineers GitHub username",
+              name: "engineerGitHub",
+            },
+            {
+              type: "list",
+              message: "Would you like to input another engineer?",
+              choices: ["yes", "no"],
+              name: "engineerYesNo",
+            },
+          ])
+          .then((answers) => {
+            engineerYesNo = answers.engineerYesNo;
+            console.log(engineerYesNo);
+            if (engineerYesNo === "yes") {
+              engineerYes();
+            } else {
+              internPrompt();
+            }
+          });
+      } else {
+        internPrompt();
+      }
+    };
+    engineerYes();
+  });
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
-
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
